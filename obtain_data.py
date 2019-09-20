d56e6f5ae4c9e8e5a3bc7aca14640317b7fd9504
Rasmus Haugaard <rasmus.l.haugaard@gmail.com>
2019-09-20 15:33:02 +0200

    .

--- obtain_data.py
+++ obtain_data.py
@@ -18,7 +18,7 @@ def cb(img):
 rospy.init_node('listener', anonymous=True)
 rospy.Subscriber('/basler/caros_camera/left/image_rect_color', Image, cb)
 
-for conf_id in range(1):
+for conf_id in range(25):
     conf_img = plt.imread("confs/{}.png".format(conf_id))
     plt.figure('Conf {}'.format(conf_id), figsize=(15, 10))
     plt.imshow(conf_img)
@@ -26,7 +26,7 @@ for conf_id in range(1):
     plt.tight_layout(pad=0)
     plt.show()
 
-    for img_id in range(2):
+    for img_id in range(10):
         input("Conf {}, image {}".format(conf_id, img_id))
         file_path = "dataset/{}_{}.png".format(conf_id, img_id)
         assert not os.path.exists(file_path), 'file already exists'
